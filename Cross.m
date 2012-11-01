e85155bd2e5582ec460c2c097e2025c7528286bb
Giovanni Charles <giovanni.charles@gmail.com>
2012-11-01 13:13:18 +0000

    finished

--- Cross.m
+++ Cross.m
@@ -14,18 +14,35 @@ end
 
 folds = 10;
 fold_size = round(length(y)/10);
-predictions = zeros(folds, length(y));
+predictions = zeros(length(y),1);
 
 %cross validation
 for fold = 1:folds,
     s = ((fold - 1) * fold_size) + 1;
     e = s + fold_size;
-    train_x = x(s:e,:);
+    train_x = x;
+    train_x(s:e,:) = [];
     for emotion = 1:6,
-        train_targets = binary_targets(s:e,emotion);
+        train_targets = binary_targets(:,emotion);
+        train_targets(s:e) = [];
         t{emotion} = DECISION_TREE_LEARNING(train_x, attributes, train_targets);
     end
-    test_x = x;
-    test_x(s:e,:) = [];
-    predictions(fold, :) = PREDICTIONS_DEPTH(t, test_x);
+    test_x = x(s:e,:);
+    p = PREDICTIONS_DEPTH(t, test_x);
+    predictions(s:e) = p;
 end
+
+%confusion matrix
+predictions(folds*fold_size + 1:length(predictions)) = NaN;
+cm = CONFUSION_MATRIX(predictions, y);
+
+%recall rates, precision rates & f1
+recall_rates = zeros(6,1);
+precision_rates = zeros(6,1);
+f1 = zeros(6,1);
+for emotion = 1:6,
+    recall_rates(emotion) = cm(emotion,emotion) / sum(cm(emotion,:)) * 100;
+    precision_rates(emotion) = cm(emotion,emotion) / sum(cm(:,emotion)) * 100;
+    f1(emotion) = 2 * precision_rates(emotion) * recall_rates(emotion) / (precision_rates(emotion) + recall_rates(emotion));
+end
+
